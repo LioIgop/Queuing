c303fc36a0b0d672620e11e54fe019d56eb1c408
ramos.271092 <ramos.271092@LAB-B-PC21>
2023-09-15 18:19:21 +0800

    Add project files.

---
+++ Queuing/CashierWindowQueueForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Queuing
+{
+    partial class CashierWindowQueueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRefresh = new Button();
+            btnNext = new Button();
+            ListCashierQueue = new ListView();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(12, 37);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(75, 23);
+            btnRefresh.TabIndex = 0;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(12, 66);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(75, 23);
+            btnNext.TabIndex = 1;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = true;
+            // 
+            // ListCashierQueue
+            // 
+            ListCashierQueue.Location = new Point(153, 37);
+            ListCashierQueue.Name = "ListCashierQueue";
+            ListCashierQueue.Size = new Size(166, 192);
+            ListCashierQueue.TabIndex = 2;
+            ListCashierQueue.UseCompatibleStateImageBehavior = false;
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // CashierWindowQueueForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(451, 262);
+            Controls.Add(ListCashierQueue);
+            Controls.Add(btnNext);
+            Controls.Add(btnRefresh);
+            Name = "CashierWindowQueueForm";
+            Text = "CashierWindowQueueForm";
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRefresh;
+        private Button btnNext;
+        private ListView ListCashierQueue;
+        private FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ Queuing/QueuingForm.cs
@@ -0,0 +1,18 @@
+using System.Drawing.Text;
+
+namespace Queuing
+{
+    public partial class Form1 : Form
+    {
+        private CashierClass cashier;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCashier_Click(object sender, EventArgs e)
+        {
+             
+
+        }
+    }
+}
